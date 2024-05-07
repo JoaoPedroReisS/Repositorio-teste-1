9cf3971951c2b1f750fbcea56d85e20f2e1a91d0
JoaoPedroReisS <163148608+JoaoPedroReisS@users.noreply.github.com>
2024-05-07 14:18:07 +0000

    Primeira versão

---
+++ main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+void serve_client(int client_socket) {
+    char response[] = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<html><body><h1>Escrevi e sai correndo, pau no cu de quem ta lendo</h1></body></html>";
+    send(client_socket, response, sizeof(response), 0);
+}
+
+int main() {
+    int server_fd, client_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Bind the socket to localhost port 8080
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listening for incoming connections
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    // Accepting incoming connections
+    while (1) {
+        if ((client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        serve_client(client_socket);
+
+        close(client_socket);
+    }
+
+    return 0;
+}
